b21f8212df9f34692fa50924c18dfd2aeb2ac327
NH <ridoyhossain5557@gmail.com>
2020-03-02 21:29:31 -0800

    xmlhttp_request_demo

--- index.js
+++ index.js
@@ -1,49 +1,24 @@
-// let root = document.querySelector('#root');
 
-// let jumbotronDiv = document.createElement("div");
+let btn = document.querySelector('#btn');
+let p = document.querySelector('#p');
 
-// jumbotronDiv.className = 'jumbotron text-center';
-
-// jumbotronDiv.setAttribute('id', 'myId');
-
-// root.appendChild(jumbotronDiv);
-
-// let h1 = document.createElement('h1');
-// h1.className = 'display-4'
+const xhr = new XMLHttpRequest();
 
 
+url = 'https://jsonplaceholder.typicode.com/users'
+
+btn.addEventListener('click', function() {
+     xhr.onreadystatechange = () => {
+        p.innerHTML = xhr.response;
+    }
+
+    xhr.open('GET', url);
+    xhr.send();
 
 
-let input = document.querySelector('#inputField');
-let ul = document.querySelector('#nameList');
-
-input.addEventListener('keypress', (event) => {
-
-        if(event.keyCode === 13) {
-            // let name = input.value;
-            // or
-            let name = event.target.value;
-            createLi(name);
-            input.value = "";
-        }
-    
 });
 
-function createLi(name) {
-    let li = document.createElement('li');
-    li.className = 'list-group-item d-flex';
-    li.innerHTML = name;
-    ul.appendChild(li);
+// xhr.onreadystatechange = () =>{
+//     console.log(xhr.response);
+// }
 
-    let span = document.createElement('span');
-    span.innerText = 'X';
-    span.className = 'ml-auto';
-    span.style.color = 'red';
-    span.style.cursor = 'pointer'
-    li.appendChild(span);
-    span.addEventListener('click', () => {
-        ul.removeChild(li);
-    });
-    
-
-}
